9f66d8f2ad2d6a06c15c3ff1eb065557bdde3b4c
Vaibhav9889 <80984167+SaketKr-On-Git@users.noreply.github.com>
2021-12-28 20:26:24 +0530

    added files

--- project/app.py
+++ project/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
-
+from flask import Flask,render_template,url_for,redirect
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello!<h1>'
+    return render_template("index.html")
+
+if __name__=='__main__':
+     app.run(debug=True)
