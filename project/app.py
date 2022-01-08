c3bf59143e54201a2a8dfac54d0e7f9e207178dd
Vaibhav9889 <80984167+SaketKr-On-Git@users.noreply.github.com>
2022-01-08 15:33:50 +0530

    added backend files

--- project/app.py
+++ project/app.py
@@ -1,9 +1,49 @@
+from spacy_summarization import text_summarizer
+from gensim.summarization import summarize
+from nltk_summarization import nltk_summarizer
+import time
+import spacy
+nlp = spacy.load('en')
+
+
+# Web Scraping Pkg
+from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+from urllib import urlopen
+
+# Sumy Pkg
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+# Sumy 
+def sumy_summary(docx):
+	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
+	lex_summarizer = LexRankSummarizer()
+	summary = lex_summarizer(parser.document,3)
+	summary_list = [str(sentence) for sentence in summary]
+	result = ' '.join(summary_list)
+	return result
+
 from flask import Flask,render_template,url_for,redirect
 app=Flask(__name__)
 
+# Reading Time
+def readingTime(mytext):
+	total_words = len([ token.text for token in nlp(mytext)])
+	estimatedTime = total_words/200.0
+	return estimatedTime
+
+# Fetch Text From Url
+def get_text(url):
+	page = urlopen(url)
+	soup = BeautifulSoup(page)
+	fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+	return fetched_text
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html")  
 
 if __name__=='__main__':
      app.run(debug=True)
